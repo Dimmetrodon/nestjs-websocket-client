7b1beade50a8cad12a3d9c4934759596a583320b
Дмитрий Егошин <egoshindm@mail.ru>
2023-05-17 16:01:10 +0300

    deleted extra comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,42 +13,10 @@ import { Server } from 'socket.io';
 import { AppGateway } from './app.gateway';
 import { GrpcMethod } from '@nestjs/microservices';
 
-export interface DtoMessageToAll {
-  text: string;
-}
-
-export interface DtoMessageToRoom {
-  room: string;
-  text: string;
-}
-
 @Controller('messages')
-export class MessagesControllerGrpc {
+export class MessagesControllerGrpc 
+{
   constructor(private appGateway: AppGateway) {}
-
-  @Post('messagetoall')
-  createMessageToAll(@Body() dto: DtoMessageToAll) {
-    if (this.appGateway.wss) {
-      this.appGateway.wss.emit('msgToClient', dto.text);
-      console.log('POST REQUEST TO SECOND SERVER TEST PASSED');
-    } else {
-      console.log('WebSocket server is not initialized yet');
-    }
-  }
-
-  @Post('messagetoroom')
-  createMessageToRoom(@Body() dto: DtoMessageToRoom) 
-  {
-    if (this.appGateway.wss) 
-    {
-      this.appGateway.wss.to(dto.room).emit('msgToClient', dto.text);
-      console.log(`send message ${dto.text} to room ${dto.room}`);
-    }
-     else 
-    {
-      console.log('WebSocket server is not initialized yet');
-    }
-  }
 
   @GrpcMethod('messageServiceGrpc', 'CreateMessageToRoomGrpc')
   createMessageToRoomGrpc(data: {room: string, text: string}) 
